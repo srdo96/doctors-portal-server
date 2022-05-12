63c81982bc31e81879cac02596b969ccb4a5a534
Sakib Rahman <seu.sakib@gmail.com>
2022-05-12 17:30:36 +0600

    server init

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tiz3z.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client
+      .db("doctors_portal")
+      .collection("services");
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log("Listening to port -> ", port);
+});
