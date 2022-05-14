770fda470ca51af7934e5508fd659941cf4d5b59
Sakib Rahman <seu.sakib@gmail.com>
2022-05-14 17:10:20 +0600

    available API

--- index.js
+++ index.js
@@ -35,6 +35,27 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/available", async (req, res) => {
+      const date = req.query.date || "May 14, 2022";
+
+      // step 1: get all services
+      const services = await serviceCollection.find().toArray();
+
+      // step 2: get the booking of that day
+      const query = { date: date };
+      const bookings = await bookingCollection.find(query).toArray();
+
+      // step 3: foreach service, find bookings for the service
+      services.forEach((service) => {
+        const serviceBookings = bookings.filter(
+          (b) => b.service === service.name
+        );
+        const booked = serviceBookings.map((s) => s.slot);
+        const available = service.slots.filter((s) => !booked.includes(s));
+        service.available = available;
+      });
+      res.send(services);
+    });
     /**
      * API Naming Convention
      *
